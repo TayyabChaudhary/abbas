fbfc463e0d19de82dc02caa0088a60e3d945e17f
Tayyeb Chaudhary <chtayyeb250@gmail.com>
2020-03-22 13:28:43 +0500

    abbas site

---
+++ src/Components/Sections/Section1/section1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+export default function section1() {
+
+    return (
+        <div>
+            <h1 className="text-blue-600">Hello World Tayyeb</h1>
+            <button className="button bg-blue-400 p-4 text-white text-2xl ml-40 rounded py-4 hover:bg-blue-900">
+                Click Heree
+            </button>
+        </div>
+    )
+}
---
+++ src/Container/Mainpage/mainpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section1 from '../../Components/Sections/Section1/section1'
+
+export default function mainpage() {
+    return (
+        <div>
+           
+
+           <Section1/>
+        </div>
+    )
+}
